aa48156d2a8e190324b7feb7b590281fbf544a20
Suryansh Tripathi <Suryansh1004@gmail.com>
2022-06-20 16:45:46 +0530

    Add files via upload

---
+++ src/app/studio-upload/studio-upload.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
+
+import { StudioUploadComponent } from './studio-upload.component';
+
+describe('StudioUploadComponent', () => {
+  let component: StudioUploadComponent;
+  let fixture: ComponentFixture<StudioUploadComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ StudioUploadComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StudioUploadComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/studio-upload/studio-upload.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-studio-upload',
+  templateUrl: './studio-upload.component.html',
+  styleUrls: ['./studio-upload.component.css']
+})
+export class StudioUploadComponent implements OnInit {
+  Urllist:New[]=
+  [new New("ABC.com")];
+
+  isShowDiv = true;  
+     
+  toggleDisplayDiv() {  
+    this.isShowDiv = !this.isShowDiv;  
+  }
+
+  constructor() {
+
+
+  }
+
+  ngOnInit(): void {
+  }
+  onSubmit(contactForm:any){
+    console.log(contactForm.value);
+    // alert(contactForm.value);
+
+}
+}
+export class New{
+  public Video_url:string;
+
+   
+  constructor(Video_url:string) {
+
+    this.Video_url=Video_url;
+  }
+}
+
